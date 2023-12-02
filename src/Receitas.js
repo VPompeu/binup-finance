fbbc86c54824abfe2bbf989c9a3a60c9aee24bb5
Victor Pompeu <vd.pompeu@gmail.com>
2023-12-02 01:38:44 -0300

    atualizado

--- src/Receitas.js
+++ src/Receitas.js
@@ -1,125 +1,148 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
-    const [titulo, setTitulo] = useState('');
-    const [valor, setValor] = useState('');
-    const [receitas, setReceitas] = useState([]);
-    const [despesas, setDespesas] = useState([]);
-    const [saldo, setSaldo] = useState(0);
-    const [modalReceita, setModalReceita] = useState(false);
-    const [modalDespesa, setModalDespesa] = useState(false);
+function App() {
+  const [modalType, setModalType] = useState(null);
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState('');
+  const [transactions, setTransactions] = useState([]);
 
-    const adicionarReceita = () => {
-        const novaReceita = {
-          titulo,
-          valor: parseFloat(valor),
-          tipo: 'receita',
-        };
-        setReceitas([...receitas, novaReceita]);
-        calcularSaldo();
-        setModalReceita(false);
-        setTitulo('');
-        setValor('');
-     };
-    
-     const adicionarDespesa = () => {
-        const novaDespesa = {
-          titulo,
-          valor: parseFloat(valor),
-          tipo: 'despesa',
-        };
-        setDespesas([...despesas, novaDespesa]);
-        calcularSaldo();
-        setModalDespesa(false);
-        setTitulo('');
-        setValor('');
-     };
+  useEffect(() => {
+    const storedTransactions = JSON.parse(localStorage.getItem('transactions')) || [];
+    setTransactions(storedTransactions);
+  }, []);
 
-     const calcularSaldo = () => {
-        const somaReceitas = receitas.reduce(
-          (acc, item) => acc + item.valor,
-          0
-        );
-        const somaDespesas = despesas.reduce(
-          (acc, item) => acc + item.valor,
-          0
-        );
-        const saldoCalculado = somaReceitas - somaDespesas;
-        setSaldo(saldoCalculado);
-     };
+  const handleAddTransaction = () => {
+    if (title && value) {
+      const newTransaction = {
+        title,
+        value: parseFloat(value),
+        type: modalType,
+      };
 
-     return (
-        <div className="App">
-          <h1>Página de Finanças</h1>
-          <button onClick={() => setModalReceita(true)}>
-            Cadastrar Receita
-          </button>
-          <button onClick={() => setModalDespesa(true)}>
-            Cadastrar Despesa
-          </button>
-          <button onClick={calcularSaldo}>
-            Calcular Saldo
-          </button>
-          <table>
-            <thead>
-              <tr>
-                <th>Título</th>
-                <th>Valor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {[...receitas, ...despesas].map((item, index) => (
-                <tr key={index} style={{ color: item.tipo === 'receita' ? 'green' : 'red' }}>
-                 <td>{item.titulo}</td>
-                 <td>{item.valor}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h2>Saldo: {saldo}</h2>
-    
-          {modalReceita && (
-            <div className="modal">
-              <h2>Cadastrar Receita</h2>
-              <input
-                type="text"
-                placeholder="Título"
-                value={titulo}
-                onChange={(e) => setTitulo(e.target.value)}
-              />
-              <input
-                type="number"
-                placeholder="Valor"
-                value={valor}
-                onChange={(e) => setValor(e.target.value)}
-              />
-              <button onClick={adicionarReceita}>Adicionar</button>
-              <button onClick={() => setModalReceita(false)}>Fechar</button>
-            </div>
-          )}
-    
-          {modalDespesa && (
-            <div className="modal">
-              <h2>Cadastrar Despesa</h2>
-              <input
-                type="text"
-                placeholder="Título"
-                value={titulo}
-                onChange={(e) => setTitulo(e.target.value)}
-              />
-              <input
-                type="number"
-                placeholder="Valor"
-                value={valor}
-                onChange={(e) => setValor(e.target.value)}
-              />
-              <button onClick={adicionarDespesa}>Adicionar</button>
-              <button onClick={() => setModalDespesa(false)}>Fechar</button>
-            </div>
-          )}
-        </div>
-      );
+      const updatedTransactions = [...transactions, newTransaction];
+      setTransactions(updatedTransactions);
+
+      localStorage.setItem('transactions', JSON.stringify(updatedTransactions));
+
+      setTitle('');
+      setValue('');
+      setModalType(null);
     }
-    
-    export default App;
-    
+  };
+
+  const handleOpenModal = (type) => {
+    setModalType(type);
+  };
+
+  const handleRemoveTransaction = (index) => {
+    const updatedTransactions = [...transactions];
+    updatedTransactions.splice(index, 1);
+
+    setTransactions(updatedTransactions);
+    localStorage.setItem('transactions', JSON.stringify(updatedTransactions));
+  };
+
+  const calculateTotalBalance = () => {
+    let totalBalance = 0;
+
+    transactions.forEach((transaction) => {
+      if (transaction.type === 'receita') {
+        totalBalance += transaction.value;
+      } else if (transaction.type === 'despesa') {
+        totalBalance -= transaction.value;
+      }
+    });
+
+    return totalBalance;
+  };
+
+  return (
+    <div className="App">
+      <h1>Receitas e Despesas</h1>
+      <button className="btn btn-success" onClick={() => handleOpenModal('receita')}>
+        Cadastrar Receita
+      </button>
+      <button className="btn btn-danger" onClick={() => handleOpenModal('despesa')}>
+        Cadastrar Despesa
+      </button>
+
+      <table className="table mt-3">
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Tipo</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction, index) => (
+            <tr key={index} className={transaction.type === 'despesa' ? 'table-danger' : 'table-success'}>
+              <td>{transaction.title}</td>
+              <td>{transaction.value}</td>
+              <td>{transaction.type}</td>
+              <td>
+                <button className="btn btn-danger" onClick={() => handleRemoveTransaction(index)}>
+                  Remover
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="modal" style={{ display: modalType ? 'block' : 'none' }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">
+                {modalType === 'receita' ? 'Cadastrar Receita' : 'Cadastrar Despesa'}
+              </h4>
+              <button type="button" className="close" onClick={() => setModalType(null)}>
+                &times;
+              </button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="form-group">
+                  <label>Título:</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Valor:</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary" onClick={handleAddTransaction}>
+                Cadastrar
+              </button>
+              <button type="button" className="btn btn-secondary" onClick={() => setModalType(null)}>
+                Fechar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="total-balance">
+        <h3>Saldo Total: {calculateTotalBalance()}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
